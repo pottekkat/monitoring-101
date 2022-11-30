2e7786a067845e1ea9442cfc3e06d277f963cc0c
Navendu Pottekkat <navendu@apache.org>
2022-11-30 10:59:06 +0530

    add python app
    Signed-off-by: Navendu Pottekkat <navendu@apache.org>

---
+++ python-app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI()
+
+hello = {"en": "Hello", "fr": "Bonjour", "es": "Hola", "ml": "ഹലോ"}
+
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/{lang}")
+async def get_hello(lang):
+    return {"message": hello[lang]}
